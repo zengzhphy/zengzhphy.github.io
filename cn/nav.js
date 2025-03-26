18535b683ec016f8fea2319b4bf986f1410ad8d7
Zhi Zeng <119023696+zengzhphy@users.noreply.github.com>
2025-03-26 23:58:12 +0000

    Update nav.js

--- cn/nav.js
+++ cn/nav.js
@@ -1,8 +1,8 @@
 document.writeln("		<nav>");
 document.writeln("    		<ul>");
 document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
-document.writeln("        		<li><a href=\"undergraduate.html\">本科</a></li>");
-document.writeln("        		<li><a href=\"postgraduate.html\">研究生</a></li>");
+document.writeln("        		<li><a href=\"BSc.html\">本科</a></li>");
+document.writeln("        		<li><a href=\"MSc.html\">硕士</a></li>");
 <!--这个叹号杠杠的作用好像是注释document.writeln("        		<li><a href=\"links.html\">链接</a></li>");-->
 document.writeln("    		</ul>");
 document.writeln("		</nav>");
