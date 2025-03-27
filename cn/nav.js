2d7ac615d483124eaa6fc4fb8732315c0925c6a5
Zhi Zeng <119023696+zengzhphy@users.noreply.github.com>
2025-03-27 00:01:58 +0000

    Update nav.js

--- cn/nav.js
+++ cn/nav.js
@@ -1,6 +1,6 @@
 document.writeln("		<nav>");
 document.writeln("    		<ul>");
-document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
+document.writeln("        		<li><a href=\"index.html\">主页</a></li>");
 document.writeln("        		<li><a href=\"BSc.html\">本科</a></li>");
 document.writeln("        		<li><a href=\"MSc.html\">硕士</a></li>");
 <!--这个叹号杠杠的作用好像是注释document.writeln("        		<li><a href=\"links.html\">链接</a></li>");-->
